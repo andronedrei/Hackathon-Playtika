8ff7b9f684e1aa73f393e6de31642e45bcb6ae38
Berceagod <102349260+Berceagod@users.noreply.github.com>
2025-04-05 01:36:49 +0300

    Primele2nivele

--- Assets/Bercea/ScripturiBercea/Camera/ScriptCamera.cs
+++ Assets/Bercea/ScripturiBercea/Camera/ScriptCamera.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
 
-public class ScriptCamera : MonoBehaviour
+
+[RequireComponent(typeof(Camera))]
+public class StaticLevelCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Level View Settings")]
+    [Tooltip("Adjust this value to change the zoom level. Smaller values zoom IN, larger values zoom OUT.")]
+    public float levelOrthographicSize = 10f; 
+
+    private Camera cam;
+
+    void Awake()
     {
         
+        cam = GetComponent<Camera>();
+        cam.orthographic = true;
+        ApplyOrthographicSize();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ApplyOrthographicSize()
     {
-        
+        if (cam != null)
+        {
+            cam.orthographicSize = levelOrthographicSize;
+        }
+        else
+        {
+            Debug.LogError("StaticLevelCamera: Camera component not found!");
+        }
     }
+
+  
+    #if UNITY_EDITOR
+    void OnValidate()
+    {
+       
+        if (cam == null)
+        {
+            cam = GetComponent<Camera>();
+        }
+        ApplyOrthographicSize();
+    }
+    #endif
 }
