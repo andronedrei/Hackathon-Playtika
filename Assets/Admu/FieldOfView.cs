823587a6e58c6c7175378fde3d6cd2b948d8f4a1
Andrei <andrei.androne.05@gmail.com>
2025-04-05 13:03:16 +0300

    FOR COP PATH NOW TAKE POINT FROM PREFAB OF MY DIR

--- Assets/Admu/FieldOfView.cs
+++ Assets/Admu/FieldOfView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -6,12 +7,14 @@ using UnityEngine.UIElements;
 
 public class FieldOfView : MonoBehaviour
 {
-    [SerializeField] protected uint view_distance;
-    [SerializeField] protected float fov; // fov ca unghi
-    [SerializeField] protected int ray_count; // ar trebui sa fie macar 1
     [SerializeField] protected Material fov_material;
-
     private Mesh fov_mesh;
+
+    protected uint view_distance = 5;
+    protected float fov = 30; // fov ca unghi
+    protected int ray_count = 20; // ar trebui sa fie macar 1
+
+    private float angle_offset = 0f;
 
     void Start()
     {
@@ -22,17 +25,15 @@ public class FieldOfView : MonoBehaviour
 
     void Update()
     {
-        Vector3 fov_origin = transform.position;
-
         ray_count = Mathf.Max(1, ray_count);
         float angle_step = fov / ray_count;
-        float start_angle = -fov / 2f;
+        float start_angle = -fov / 2f + angle_offset;
 
         Vector3[] vertices = new Vector3[ray_count + 2]; // vertice ("puncte" in scena)
         Vector2[] uv = new Vector2[vertices.Length]; // coordonate texturi (cum mapezi 2D => 3D)
         int[] triangles = new int[ray_count * 3]; // grupari de cate 3 vertice formand triunghiurile
 
-        vertices[0] = fov_origin;
+        vertices[0] = Vector3.zero;
 
         for (int i = 0; i <= ray_count; i++)
         {
@@ -42,13 +43,13 @@ public class FieldOfView : MonoBehaviour
             Vector3 dir = new(Mathf.Cos(angleRad), Mathf.Sin(angleRad));  // vector trigonometric
 
             // verificam cu un ray cast daca am intersecta un alt obiect
-            RaycastHit2D check_hit = Physics2D.Raycast(fov_origin, dir, view_distance);
+            RaycastHit2D check_hit = Physics2D.Raycast(transform.position, dir, view_distance);
             if (check_hit.collider == null) {
                 // nu am lovit nimic, randam pana la "view distance"
                 vertices[i + 1] = dir * view_distance;
             } else {
                 // am lovit ceva, randam pana la obiect
-                vertices[i + 1] = check_hit.point - (Vector2)fov_origin;
+                vertices[i + 1] = check_hit.point - (Vector2)transform.position;
             }
             
         }
@@ -63,5 +64,14 @@ public class FieldOfView : MonoBehaviour
         fov_mesh.vertices = vertices;
         fov_mesh.uv = uv;
         fov_mesh.triangles = triangles;
+    }
+
+    // fa campul vizual sa fie indreptat catre o directie
+    public void SetDirection(Vector2 dir) {
+        angle_offset = Mathf.Atan2(dir.y, dir.x);
+    }
+
+    public void SetOrigin(Vector2 point) {
+        //transform.position = (Vector2)point;
     }
 }
