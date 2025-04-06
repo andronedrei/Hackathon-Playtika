08f41acb165444281cd29d67b3fec603e6794327
Bercea Mihnea-Cristian <mihnea910@gmail.com>
2025-04-06 03:45:08 +0300

    Test222

--- Assets/Admu/Player.cs
+++ Assets/Admu/Player.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
+using System.Collections; // Ne trebuie pentru IEnumerator
 
-// Enum-urile raman la fel
 public enum PoolThiefType { Type_1, Type_2 }
 public enum ControlType { Arrows, WASD }
 
@@ -13,92 +13,139 @@ public class Player : MovableEntity
 
     private Animator myAnimator;
     private SpriteRenderer mySpriteRender;
-    private Vector2 lastMovementDirection = Vector2.down; // Adaugam pentru idle corect
+    private Vector2 lastMovementDirection = Vector2.down;
 
-    private void Awake() // Este mai bine sa folosesti Awake pentru GetComponent
+    AudioSource audioSource;
+
+    private Coroutine fadeCoroutine;
+    [SerializeField] private float fadeDuration = 0.3f;
+
+    private void Awake()
     {
-        // Incearca sa gasesti componentele si in copii daca structura ta e GFX/Collision
         myAnimator = GetComponentInChildren<Animator>();
         mySpriteRender = GetComponentInChildren<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
 
-        // Verificari pentru a te asigura ca au fost gasite
         if (myAnimator == null) Debug.LogError("Animator component not found!", this);
         if (mySpriteRender == null) Debug.LogError("SpriteRenderer component not found!", this);
     }
 
     protected override void HandleMovement()
     {
-        movement_dir = Vector2.zero; // Resetam directia
+        movement_dir = Vector2.zero;
 
         bool up = Input.GetKey(movement_keys[0]);
         bool down = Input.GetKey(movement_keys[1]);
         bool left = Input.GetKey(movement_keys[2]);
         bool right = Input.GetKey(movement_keys[3]);
 
-        // Logica pentru sus/jos ramane la fel
         if (up)
             movement_dir.y += 1.0f;
         if (down)
             movement_dir.y -= 1.0f;
 
-        // --- COREctIE AICI ---
         if (right)
-        { // Adauga acolada de deschidere
+        {
             mySpriteRender.flipX = false;
             movement_dir.x += 1.0f;
-        } // Adauga acolada de inchidere
+        }
 
         if (left)
-        { // Adauga acolada de deschidere
+        {
             mySpriteRender.flipX = true;
             movement_dir.x -= 1.0f;
-        } // Adauga acolada de inchidere
-        // --- Sfarsit COREctIE ---
+        }
 
-        // Normalizam pentru viteza consistenta
         movement_dir = movement_dir.normalized;
 
-        // Aplicam miscarea (din MovableEntity, probabil transform.Translate)
         TranslateEntity();
-
-        // Actualizam animatorul
         UpdateAnimator();
     }
 
-    // Metoda separata pentru actualizarea animatorului
     private void UpdateAnimator()
     {
-        if (myAnimator == null) return; // Verificare siguranta
+        if (myAnimator == null) return;
 
         bool isMoving = movement_dir.sqrMagnitude > 0.01f;
-        myAnimator.SetBool("IsMoving", isMoving); // Adauga si parametrul IsMoving in Animator
-        Debug.Log($"Frame: {Time.frameCount} | isMoving = {isMoving} | Current movement_dir: {movement_dir}");
+        myAnimator.SetBool("IsMoving", isMoving);
+
+        bool wasMoving = audioSource.isPlaying;
+
         if (isMoving)
         {
             myAnimator.SetFloat("MoveX", movement_dir.x);
             myAnimator.SetFloat("MoveY", movement_dir.y);
-            lastMovementDirection = movement_dir; // Salveaza ultima directie valida
+            lastMovementDirection = movement_dir;
+
+            if (!wasMoving)
+            {
+                StartFadeIn();
+            }
         }
-        else // Idle
+        else
         {
-            // Foloseste ultima directie pentru idle corect
             myAnimator.SetFloat("MoveX", lastMovementDirection.x);
             myAnimator.SetFloat("MoveY", lastMovementDirection.y);
+
+            if (wasMoving)
+            {
+                StartFadeOut();
+            }
         }
     }
 
+    private void StartFadeIn()
+    {
+        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(FadeIn());
+    }
+
+    private void StartFadeOut()
+    {
+        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        audioSource.volume = 0f;
+        if (!audioSource.isPlaying) audioSource.Play();
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            audioSource.volume = Mathf.Clamp01(elapsed / fadeDuration);
+            yield return null;
+        }
+
+        audioSource.volume = 1f;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float startVolume = audioSource.volume;
+        float elapsed = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, 0f, elapsed / fadeDuration);
+            yield return null;
+        }
+
+        audioSource.volume = 0f;
+        audioSource.Stop();
+    }
 
     void Start()
     {
-        // Logica din Start ramane la fel pentru setarea tastelor si subscribe
-        // movement_dir = Vector2.zero; // Deja setat in Awake/HandleMovement
-        speed = base_speed; // Asigura-te ca speed si base_speed sunt initializate corect in MovableEntity
+        speed = base_speed;
 
-        SubscribeToTimeManager(); // Muta subscribe aici daca TimeManager e gata doar in Start
-        SetupKeys(); // Seteaza tastele
+        SubscribeToTimeManager();
+        SetupKeys();
     }
 
-    // SetupKeys si SubscribeToTimeManager (separate pentru claritate)
     private void SetupKeys()
     {
         switch (control_type)
@@ -107,7 +154,7 @@ public class Player : MovableEntity
                 movement_keys[0] = KeyCode.W; movement_keys[1] = KeyCode.S;
                 movement_keys[2] = KeyCode.A; movement_keys[3] = KeyCode.D;
                 break;
-            default: // Arrows ca default
+            default:
                 movement_keys[0] = KeyCode.UpArrow; movement_keys[1] = KeyCode.DownArrow;
                 movement_keys[2] = KeyCode.LeftArrow; movement_keys[3] = KeyCode.RightArrow;
                 break;
@@ -130,24 +177,8 @@ public class Player : MovableEntity
         }
     }
 
-    // folosim fixed update pentru consistenta cu atributele de fizica
-    // ATENTIE: Daca MovableEntity.TranslateEntity NU foloseste Rigidbody,
-    // chemarea din FixedUpdate poate cauza jitter. Daca MovableEntity foloseste
-    // transform.Translate, e mai bine sa chemi HandleMovement din Update().
-    // Daca MovableEntity foloseste Rigidbody.MovePosition, FixedUpdate e corect.
-    // Presupunand ca MovableEntity foloseste transform.Translate, mutam in Update:
-    /* void FixedUpdate()
+    void Update()
     {
-       // HandleMovement(); // Comentat sau sters
-    } */
-
-    void Update() // Cheama HandleMovement din Update daca folosesti transform.Translate
-    {
-        // Adauga verificarea pentru freeze si aici daca nu o face MovableEntity
-        // if (isFrozen) { /* logica de freeze */ return; }
-
         HandleMovement();
     }
-
-
-} // Sfarsitul clasei Player
+}
